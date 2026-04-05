234df7d39f43ef9929253186832ea3ddf223ed48
maaswin03 <m.a.aswin2005@gmail.com>
2026-04-05 13:13:18 +0530

    task-3 1.2

--- task-3/demo.js
+++ task-3/demo.js
@@ -1,5 +1,5 @@
-let fun = 20;
-let run = 10;
+let a = 20;
+let b = 10;
 
 function addNumber(a, b) {
   const answer = a + b;
@@ -11,5 +11,5 @@ function subNumber(a, b) {
   console.log("The answer is : ", answer);
 }
 
-subNumber(fun, run);
-addNumber(fun, run);
+subNumber(a, b);
+addNumber(a, b);
