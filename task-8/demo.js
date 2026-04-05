d53f6d1adbebfb9db9d4deb97c56333ef992de33
maaswin03 <m.a.aswin2005@gmail.com>
2026-04-05 18:57:50 +0530

    task-8 1.0

---
+++ task-8/demo.js
@@ -0,0 +1,25 @@
+let fun = 20;
+let run = 10;
+
+let api_key = "#123"
+
+function addNumber(a, b) {
+  const answer = a + b;
+}
+
+function subNumber(a, b) {
+  const answer = a - b;
+}
+
+function mulNumber(a, b) {
+  const answer = a * b;
+}
+
+function divNumber(a, b) {
+  const answer = a / b;
+}
+
+subNumber(fun, run);
+addNumber(fun, run);
+mulNumber(fun, run);
+divNumber(fun, run);
