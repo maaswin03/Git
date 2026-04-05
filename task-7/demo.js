1aa215d456984206ff9a7cc5debd38ed65abd632
maaswin03 <m.a.aswin2005@gmail.com>
2026-04-05 16:58:44 +0530

    task-7 1.3

--- task-7/demo.js
+++ task-7/demo.js
@@ -16,7 +16,12 @@ function mulNumber(a, b) {
   console.log("The answer is : ", answer);
 }
 
+function divNumber(a, b) {
+  const answer = a / b;
+  console.log("The answer is : ", answer);
+}
 
 subNumber(fun, run);
 addNumber(fun, run);
 mulNumber(fun, run);
+divNumber(fun, run);
