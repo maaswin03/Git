f520c27e80c81559c188c2af1a7c0616c38d05a2
maaswin03 <m.a.aswin2005@gmail.com>
2026-04-05 16:57:48 +0530

    task-7 1.1

--- task-7/demo.js
+++ task-7/demo.js
@@ -6,4 +6,10 @@ function addNumber(a, b) {
   console.log("The answer is : ", answer);
 }
 
+function subNumber(a, b) {
+  const answer = a - b;
+  console.log("The answer is : ", answer);
+}
+
+subNumber(fun, run);
 addNumber(fun, run);
