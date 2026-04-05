28c273770e13221ac4a4c1d70498fc7d40c9ca01
maaswin03 <m.a.aswin2005@gmail.com>
2026-04-05 12:14:49 +0530

    task-2 1.0

---
+++ task-2/demo.js
@@ -0,0 +1,12 @@
+function addNumber(a, b) {
+  const answer = a + b;
+  console.log("The answer is : ", answer);
+}
+
+function subNumber(a, b) {
+  const answer = a - b;
+  console.log("The answer is : ", answer);
+}
+
+subNumber(10, 2);
+addNumber(10, 2);
