20c2df0427502f6badabbdd6960c4da2b4d26a19
maaswin03 <m.a.aswin2005@gmail.com>
2026-04-05 15:37:01 +0530

    task-5 1.0

---
+++ task-5/demo.js
@@ -0,0 +1,15 @@
+let fun = 200;
+let run = 100;
+
+function addNumber(a, b) {
+  const answer = a + b;
+  console.log("The answer is : ", answer);
+}
+
+function subNumber(a, b) {
+  const answer = a - b;
+  console.log("The answer is : ", answer);
+}
+
+subNumber(fun, run);
+addNumber(fun, run);
